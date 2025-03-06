add5c8ed59e06b36ba00ae009933d419af20eff8
Galingdu <henglanhzaa09.12.2021@gmail.com>
2025-03-06 10:50:46 +0700

    first commit

---
+++ js/po.js
@@ -0,0 +1,114 @@
+let Item_list = [
+    {
+        id:1,
+        Name: 'Coca',
+        Price: 0.5,
+        image: '1697119150_76476-frontal2028129.jpg',
+
+    },
+    {
+        id:2,
+        Name: 'Sting',
+        Price: 0.5,
+        image: 'Sting-Energy-Drink-Berry-Blast-Flavor---320ml-_Vietnam_-Sting-Energy-1684391107_700x700.webp',
+
+    },
+    {
+        id:3,
+        Name: 'Hanuman',
+        Price: 1,
+        image: '8847108061722.jpg',
+
+    },
+    {
+        id:4,
+        Name: 'Krud',
+        Price: 1,
+        image: 'Krud-Pilsner-Beer-30Cans_NCP-330ml-01.jpg',
+
+    },
+    {
+        id:5,
+        Name: 'ABC',
+        Price: 2,
+        image: 'abc.jpg',
+
+    },
+    {
+        id:6,
+        Name: 'Sprite',
+        Price: 0.75,
+        image: 'sprite.jpg',
+
+    },
+    {
+        id:7,
+        Name: 'pepsi',
+        Price: 0.5,
+        image: 'Pepsi-lon.jpg',
+
+    },
+    {
+        id:8,
+        Name: 'Fanta',
+        Price: 0.75,
+        image: 'fanta.webp',
+
+    },
+    {
+        id:9,
+        Name: 'Coca',
+        Price: 0.5,
+        image: '1697119150_76476-frontal2028129.jpg',
+
+    },
+    {
+        id:10,
+        Name: 'Sting',
+        Price: 0.5,
+        image: 'Sting-Energy-Drink-Berry-Blast-Flavor---320ml-_Vietnam_-Sting-Energy-1684391107_700x700.webp',
+
+    },
+    {
+        id:11,
+        Name: 'Hanuman',
+        Price: 1,
+        image: '8847108061722.jpg',
+
+    },
+    {
+        id:12,
+        Name: 'Krud',
+        Price: 1,
+        image: 'Krud-Pilsner-Beer-30Cans_NCP-330ml-01.jpg',
+
+    },
+    {
+        id:13,
+        Name: 'Sprite',
+        Price: 0.75,
+        image: 'sprite.jpg',
+
+    },
+    {
+        id:14,
+        Name: 'pepsi',
+        Price: 0.5,
+        image: 'Pepsi-lon.jpg',
+
+    },
+    {
+        id:15,
+        Name: 'Fanta',
+        Price: 0.75,
+        image: 'fanta.webp',
+
+    },
+    {
+        id:16,
+        Name: 'ABC',
+        Price: 2,
+        image: 'abc.jpg',
+
+    },
+];
